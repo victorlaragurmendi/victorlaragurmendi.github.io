51dc96528ae9501fad05b564b3db101ae79407b8
victor_lg <lince00@outlook.com>
2020-11-20 03:20:17 -0500

    agregando la produccion

---
+++ build/precache-manifest.7545f9ff1331039c37a4491cee0ec875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbf5964a2564214981739f54a1a8e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa171253ebb510965c5c",
+    "url": "/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "5ac44406f2029712fc83",
+    "url": "/static/css/main.26ccee0d.chunk.css"
+  },
+  {
+    "revision": "fa171253ebb510965c5c",
+    "url": "/static/js/2.5a7f8f9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5a7f8f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac44406f2029712fc83",
+    "url": "/static/js/main.808207ed.chunk.js"
+  },
+  {
+    "revision": "b31c40babf8aa45934b6",
+    "url": "/static/js/runtime-main.eaeaa024.js"
+  }
+]);
